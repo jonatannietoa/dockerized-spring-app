9f0d3b8c27df10e58c4248d9bb10dfeb207394ab
jonatannietoa <jonatannietoa@gmail.com>
2016-06-19 14:04:10 +0200

    Initial Commit

---
+++ src/main/java/com/lepsec/DockerizedApp.java
@@ -0,0 +1,12 @@
+package com.lepsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizedApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizedApp.class, args);
+	}
+
+}
---
+++ src/main/java/com/lepsec/controllers/IndexController.java
@@ -0,0 +1,29 @@
+package com.lepsec.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by jonatannietoa on 19/06/2016.
+ */
+@RestController
+public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        try {
+            return "Spring Dockerized App, the host name is: " + InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            if (logger.isErrorEnabled()){
+                logger.error("Index request error", e.getMessage());
+            }
+            return e.getMessage();
+        }
+    }
+}
